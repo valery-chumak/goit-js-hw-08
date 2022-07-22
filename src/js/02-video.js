9954fdf1d5a3099cdb09324f4f5b2e14ea68dfe4
valery-chumak <blossssom88@gmail.com>
2022-07-22 21:53:35 +0300

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,26 @@
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player'
 const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
 
     player.on('play', function(event) {
         console.log('played the video!');
-        console.log(event.currentTarget);
+        console.log(event.currentTarget.value);
     });
 
     player.getVideoTitle().then(function(title) {
         console.log('title:', title);
     });
 
-    player.on('timeupdate', function(event) {
-        // localStorage.setItem("videoplayer-current-time", event.target.elements.);
-        
+    player.on('timeupdate', throttle(function(time) {
+        localStorage.setItem("videoplayer-current-time", time.seconds);
+    },1000));
+
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function() {
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                assert("the time was less than 0 or greater than the video’s duration");
+                break;
+        }
     });
