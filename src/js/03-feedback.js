10b2c3470a14b04e76842ad6f511f37d1a27992d
valery-chumak <blossssom88@gmail.com>
2022-07-24 14:27:03 +0300

    fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-// import throttle from "lodash.throttle";
+import throttle from "lodash.throttle";
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input');
 const textarea = document.querySelector('textarea');
@@ -6,60 +6,36 @@ const STORAGE_KEY = 'feedback-form-state';
 
 // form.addEventListener('input', onFormInput);
 form.addEventListener('submit', onFormSubmit);
-email.addEventListener('input', onEmailInput);
-textarea.addEventListener('input', OnTextareaInput);
+email.addEventListener('input', throttle(onEmailInput, 500));
+textarea.addEventListener('input', throttle(OnTextareaInput, 500));
 let comment = {};
 populateComment();
-// function onFormInput(event) {
-//     const email = event.currentTarget.value;
-//     comment.email = email;
-//     const message = event.currentTarget.value;
-//     comment.message = message;
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(comment));
-//     console.log(comment);
-// }
+
 function onEmailInput(event) {
-    const email = event.currentTarget.value;
+    const email = event.target.value;
     comment.email = email;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(comment));
     
 }
 function OnTextareaInput(event){
-    const message = event.currentTarget.value;
-   
+    const message = event.target.value;
     comment.message = message;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(comment));
-   
 }
 
 function onFormSubmit(event) {
     event.preventDefault();
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(comment));
     event.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY)
-    comment.email = "";
-    comment.message = "";
+    localStorage.removeItem(STORAGE_KEY);
 }
 function populateComment() {
     const savedComment = localStorage.getItem(STORAGE_KEY);
     const savedCommentParsed = JSON.parse(savedComment);
-    // const savedEmail = savedCommentParsed.email;
-    // const savedMessage = savedCommentParsed.message;
+
     
     if (savedComment) {
         console.log(savedComment);
         email.value = savedCommentParsed.email;
         textarea.value = savedCommentParsed.message;
     }
-    // else if (savedEmail && !savedMessage) {
-    //     email.value = savedEmail;
-    //     textarea.value = "";
-    // }else if(!savedEmail && savedMessage){
-    //     email.value = "";
-    //     textarea.value = savedMessage;
-    // }
-    // else {
-    //     email.value = "";
-    //     textarea.value = "";
-    // }
 }
